9783efd9e936d808afe32654c3faf61c92b55243
simsun <sunsj1231@gmail.com>
2016-12-23 04:58:25 +0800

    first commit

---
+++ app/src/main/java/com/tinkerpatch/easy_sample/MainActivity.java
@@ -0,0 +1,89 @@
+package com.tinkerpatch.easy_sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.tencent.tinker.lib.util.TinkerLog;
+import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
+import com.tinkerpatch.sdk.TinkerPatch;
+import com.tinkerpatch.sdk.server.callback.ConfigRequestCallback;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Tinker.MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.e(TAG, "I am on onCreate classloader:" + MainActivity.class.getClassLoader().toString());
+        //test resouIrce change
+        Log.e(TAG, "I am on onCreate string:" + getResources().getString(R.string.test_resource));
+        Log.e(TAG, "I am on patch onCreate");
+
+        Button requestPatchButton = (Button) findViewById(R.id.requestPatch);
+
+        requestPatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerPatch.with().fetchPatchUpdate(true);
+            }
+        });
+
+        Button requestConfigButton = (Button) findViewById(R.id.requestConfig);
+
+        requestConfigButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerPatch.with().fetchDynamicConfig(new ConfigRequestCallback() {
+
+                    @Override
+                    public void onSuccess(HashMap<String, String> configs) {
+                        TinkerLog.w(TAG, "request config success, config:" + configs);
+                    }
+
+                    @Override
+                    public void onFail(Exception e) {
+                        TinkerLog.w(TAG, "request config failed, exception:" + e);
+                    }
+                }, true);
+            }
+        });
+
+        Button cleanPatchButton = (Button) findViewById(R.id.cleanPatch);
+
+        cleanPatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TinkerPatch.with().cleanPatch();
+            }
+        });
+
+        Button killSelfButton = (Button) findViewById(R.id.killSelf);
+
+        killSelfButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShareTinkerInternals.killAllOtherProcess(getApplicationContext());
+                android.os.Process.killProcess(android.os.Process.myPid());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        Log.e(TAG, "I am on onResume");
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e(TAG, "I am on onPause");
+        super.onPause();
+    }
+}
---
+++ app/src/main/java/com/tinkerpatch/easy_sample/SampleApplication.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Shengjie Sim Sun
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.tinkerpatch.easy_sample;
+
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.tinkerpatch.sdk.TinkerPatch;
+import com.tinkerpatch.sdk.loader.TinkerPatchApplicationLike;
+
+public class SampleApplication extends Application {
+    private static final String TAG = "Tinker.SampleApplication";
+
+    public SampleApplication() {
+
+    }
+
+    @Override
+    public void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        //开始检查是否有补丁，这里配置的是每隔访问3小时服务器是否有更新。
+        TinkerPatch.init(TinkerPatchApplicationLike.getTinkerPatchApplicationLike())
+            .setPatchRollbackOnScreenOff(true)
+            .setPatchRestartOnSrceenOff(true);
+    }
+}
